2497c45199a7edbf4c964af0130f9adb1c53a468
TimiMaliki <malikitimi@gmail.com>
2023-06-23 01:26:05 +0100

    npm run build

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,17 +1,74 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+import { useState } from 'react'
 
 
 
 
 function App() {
+  const [showAddTask,setshowAddTask] = useState(false)
+  const [tasks, setTask] = useState(
+    [
+       {
+          id: 1,
+          text: 'Going to code at IT plaza',
+          day: ' jun 20, 9:00am',
+          reminder: true,
+       },
+
+       {
+          id: 2,
+          text: 'Going to gym',
+          day: ' jun 20, 5:30pm',
+          reminder: true,
+       },
+
+       {
+          id: 3,
+          text: 'Going to code at home 9:30pm',
+          day: ' jun 20, 9:00pm',
+          reminder: false,
+       }
+
+    ]
+ )
+  //Add Tasks
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newAddedTask = { id, ...task }
+    setTask([...tasks, newAddedTask])
+ }
+ //Deleting Task
+
+ const deleteTask = (id) => {
+    console.log({ id });
+    setTask(tasks.filter((task) => task.id !== id))
+
+ }
+
+ //ToggleReminder
+
+ const toggleReminder = (id) => {
+    setTask(
+       tasks.map((task) =>
+          task.id === id ? { ...task, reminder: !task.reminder } : task
+
+       )
+
+    )
+ }
+ 
   return (
     <div className='container'>
- 
-      <Header />
 
-      {/* // passing a prop here
-      onAdd={() => setShowAddTask(!showAddTask)} */}
-  
+      <Header onAdd={() => setshowAddTask(!showAddTask)}showAdd={showAddTask}/>
+      {showAddTask &&<AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? <Tasks
+         task={tasks}
+         onDelete={deleteTask}
+         onToggle={toggleReminder} /> : "no task to show"}
 
     </div>
   );
--- task-tracker/src/components/Header.js
+++ task-tracker/src/components/Header.js
@@ -1,83 +1,17 @@
-import { useState } from 'react'
 import Button from '../components/Button'
-import Tasks from '../components/Tasks'
-import AddTask from '../components/AddTask'
 
-// onAdd 
-// called the prop onAdd here
-function Header({ title})
+
+function Header({ title,onAdd,showAdd})
  {
-   // const [showAddTask, setShowAddTask] = useState(false)
-   // the add button function setShowAddtask wasnt decleared
-   const [tasks, setTask] = useState(
-      [
-         {
-            id: 1,
-            text: 'Going to code at IT plaza',
-            day: ' jun 20, 9:00am',
-            reminder: true,
-         },
-
-         {
-            id: 2,
-            text: 'Going to gym',
-            day: ' jun 20, 5:30pm',
-            reminder: true,
-         },
-
-         {
-            id: 3,
-            text: 'Going to code at home 9:30pm',
-            day: ' jun 20, 9:00pm',
-            reminder: false,
-         }
-
-      ]
-   )
-   //Add Tasks
-
-   const addTask = (task) => {
-      const id = Math.floor(Math.random() * 10000) + 1
-      const newAddedTask = { id, ...task }
-      setTask([...tasks, newAddedTask])
-   }
-   //Deleting Task
-
-   const deleteTask = (id) => {
-      console.log({ id });
-      setTask(tasks.filter((task) => task.id !== id))
-
-   }
-
-   //ToggleReminder
-
-   const toggleReminder = (id) => {
-      setTask(
-         tasks.map((task) =>
-            task.id === id ? { ...task, reminder: !task.reminder } : task
-
-         )
-
-      )
-   }
-
-
-   return <div className='container'>
    
-      {/* {showAddTask && <AddTask onAdd={addTask} />} */}
-
+   return <div >
       <header className='header'>
          <h1>{title}</h1>
-         <Button color='blue' text='ADD' /> 
+         <Button color={showAdd ? 'red' : 'blue'} text={showAdd ? 'Close' : 'Add'}       
+          onClick={onAdd}/> 
       </header>
-{/*       
-          onClick={onAdd} 
-         the button onClick function */}
-         
-      {tasks.length > 0 ? <Tasks
-         task={tasks}
-         onDelete={deleteTask}
-         onToggle={toggleReminder} /> : "no task to show"}
+
+     
 
 
 
